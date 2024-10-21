6a5c424804e7fd21bad9ded16a3a29bf9de2f619
gisimfu <h.fuhrer.gva@gmail.com>
2024-10-21 00:39:57 -0400

    Task 2.2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,5 +27,15 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user.
+     * @param name description
+     */
     void setCurrentUser(String name);
+
+    /**
+     * Gets the current user.
+     * @return description
+     */
+    String getCurrentUser();
 }
